d8ad39fc477b202d90e249bb3dfd11dd05c784fc
SnezhanaTech <Eris3383@yandex.ru>
2020-09-26 18:52:30 +0300

    added slider and products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
-$ (function () {
+$ (function () { 
+ 
+    $(".rate-star").rateYo({
+      rating: 4.5,
+      starWidth: '15px',
+      readOnly: true
+    });
 
-});
+    $('.slider__inner').slick({
+      dots: false,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      cssEase: 'linear',
+      prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt=""></button>',
+      nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png"" alt=""></button>'
+    });
+
+    var mixer = mixitup('.products__box');
+
+
+  });
+
